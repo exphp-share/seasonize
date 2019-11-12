49b2e9e3847c5d4c6c430f65c9a22dbb935f2c1e
Michael Lamparski <diagonaldevice@gmail.com>
2019-11-12 00:53:20 -0500

    TABS AND SPACES AYAYAYA
    forgot to enable visible white space in my editor
    
    the autogenerated JSON files use tabs, so we'll
    use them in the handwritten files as well

--- sp_resources/th16.v1.00a.js
+++ sp_resources/th16.v1.00a.js
@@ -4,11 +4,11 @@
 			"addr":     "0x0042CE5B",
 			"code":     "C7 05 08584A00 74040000",
 			"expected": "C7 05 08584A00 86010000"
-        },
-        "Spell practice with Bombs": {
+		},
+		"Spell practice with Bombs": {
 			"addr":     "0x0042CDCD",
 			"code":     "C7 05 00584A00 05000000",
 			"expected": "C7 05 00584A00 00000000"
-        }
+		}
 	}
 }
