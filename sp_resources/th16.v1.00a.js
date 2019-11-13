4663b9dfe1d882d3116ea7128b70c31a6205c193
Michael Lamparski <diagonaldevice@gmail.com>
2019-11-12 21:09:49 -0500

    sp_resources: support TH17 and lives

--- sp_resources/th16.v1.00a.js
+++ sp_resources/th16.v1.00a.js
@@ -2,13 +2,18 @@
 	"binhacks": {
 		"Spell practice with Max Season": {
 			"addr":     "0x0042CE5B",
-			"code":     "C7 05 08584A00 74040000",
-			"expected": "C7 05 08584A00 86010000"
+			"expected": "C7 05 08584A00 86010000",
+			"code":     "C7 05 08584A00 74040000"
 		},
 		"Spell practice with Bombs": {
 			"addr":     "0x0042CDCD",
-			"code":     "C7 05 00584A00 05000000",
-			"expected": "C7 05 00584A00 00000000"
+			"expected": "C7 05 00584A00 00000000",
+			"code":     "C7 05 00584A00 05000000"
+		},
+		"Spell practice with Lives": {
+			"addr":     "0x0042CDC3",
+			"expected": "C7 05 F4574A00 00000000",
+			"code":     "C7 05 F4574A00 05000000"
 		}
 	}
 }
