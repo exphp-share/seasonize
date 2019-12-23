4e4ed8b33069a6d8ba0ff5926fa60e2fd238444c
Michael Lamparski <diagonaldevice@gmail.com>
2019-12-22 20:42:02 -0500

    Update repo.js

--- repo.js
+++ repo.js
@@ -2,7 +2,7 @@
 	"contact": "diagonaldevice@gmail.com",
 	"id": "ExpHP",
 	"patches": {
-		"sp_resources": "Spell practice with bombs and Max Season (th16)"
+		"sp_resources": "Spell practice with lives, bombs and Max Season (TH13, TH14, TH16, TH17)"
 	},
 	"servers": [
 		"https://raw.githubusercontent.com/ExpHP/thcrap-patches/master/"
